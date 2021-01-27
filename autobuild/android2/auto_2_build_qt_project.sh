817484edba7b40dafb6093aed2b1d08cea985201
nefarius2001 <nefarius2001-bitbucket@yahoo.de>
2021-01-28 00:33:26 +0100

    android-ql & autobuildstuff

---
+++ autobuild/android2/auto_2_build_qt_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Please run this script with the first parameter being the root of the repo
+if [ -z "${1}" ]; then
+    echo "Please give the path to the repository root as second parameter to this script!"
+    exit 1
+fi
+
+cd ${1}
+
+$QTDIR/bin/qmake -spec android-clang CONFIG+=$CONFIG
+/opt/android/android-ndk/prebuilt/linux-x86_64/bin/make
+$ANDROID_NDK_ROOT/prebuilt/$ANDROID_NDK_HOST/bin/make INSTALL_ROOT=android-build -f Makefile install
+$QTDIR/bin/androiddeployqt --input $(ls *.json) --output android-build --android-platform android-30 --jdk $JAVA_HOME --gradle 
--- autobuild/linux/codeQL/auto_2_build_qt_project.sh
+++ autobuild/linux/codeQL/auto_2_build_qt_project.sh
@@ -1,6 +1,4 @@
-#!/bin/sh -e
-
-# Sets up the environment for autobuild on Linux
+#!/bin/bash
 
 # Please run this script with the first parameter being the root of the repo
 if [ -z "${1}" ]; then
@@ -8,14 +6,7 @@ if [ -z "${1}" ]; then
     exit 1
 fi
 
-echo "Update system..."
-sudo apt-get -qq update
-# We don't upgrade the packages. If this is needed, just uncomment this line
-# sudo apt-get -qq -y upgrade
-echo "Install dependencies..."
-sudo apt-get update
-sudo apt install build-essential qt5-qmake qtdeclarative5-dev qt5-default qttools5-dev-tools libjack-jackd2-dev
-		
+cd ${1}
 		
 		
 echo "Building... qmake"
