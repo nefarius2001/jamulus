ab1298ecb6992e452a0154b7daac343fe185937d
nefarius2001 <nefarius2001-bitbucket@yahoo.de>
2021-02-24 02:06:16 +0100

    fix relative paths

--- distributions/build-debian-package-auto.sh
+++ distributions/build-debian-package-auto.sh
@@ -32,7 +32,7 @@ debuild -b -us -uc
 mkdir deploy
 
 if [ "${1}" = "headless" ]; then
-  mv "/../jamulus-headless*_amd64.deb" deploy/
+  mv "../jamulus-headless*_amd64.deb" deploy/
 else
-  mv "/../jamulus*_amd64.deb" deploy/
+  mv "../jamulus*_amd64.deb" deploy/
 fi
