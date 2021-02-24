65cff0aa88eb98ba9ff800a5db252402769fef0e
nefarius2001 <nefarius2001-bitbucket@yahoo.de>
2021-02-24 01:01:45 +0100

    remove project_path as parameter in script call, adding relative assumption

--- autobuild/ensure_THIS_JAMULUS_PROJECT_PATH.sh
+++ autobuild/ensure_THIS_JAMULUS_PROJECT_PATH.sh
@@ -6,18 +6,17 @@ echo "ensuring env-variable THIS_JAMULUS_PROJECT_PATH"
 ###  PARAMETERS  ###
 ####################
 
-if [ ! -z "${1}" ]; then
-    echo "THIS_JAMULUS_PROJECT_PATH from parameter"
-    THIS_JAMULUS_PROJECT_PATH="${1}"
-elif [ ! -z "${jamulus_project_path}" ]; then
+if [ ! -z "${jamulus_project_path}" ]; then
     echo "THIS_JAMULUS_PROJECT_PATH from env-variable jamulus_project_path"
     THIS_JAMULUS_PROJECT_PATH="${jamulus_project_path}"
 elif [ ! -z "${GITHUB_WORKSPACE}" ]; then
     echo "THIS_JAMULUS_PROJECT_PATH from env-variable GITHUB_WORKSPACE"
     THIS_JAMULUS_PROJECT_PATH="${GITHUB_WORKSPACE}"
 else
-    echo "Please give the path to the repository root as environment variable 'jamulus_project_path' or parameter to this script!"
-    exit 1
+    echo "THIS_JAMULUS_PROJECT_PATH relative to this script-file"
+	THIS_JAMULUS_PROJECT_PATH="$(dirname "${BASH_SOURCE[0]}")/../"
+    #echo "Please give the path to the repository root as environment variable 'jamulus_project_path' or parameter to this script!"
+    #exit 1
 fi
 
 if [ -d "$THIS_JAMULUS_PROJECT_PATH" ]; then
